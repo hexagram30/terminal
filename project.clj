42ad92909e206bf575d2283f3aa36f1d91c7dc6a
Duncan McGreggor <duncan.mcgreggor@nasa.gov>
2018-03-06 20:15:01 -0600

    Added initial files.

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject hexagram30/terminal "0.1.0-SNAPSHOT"
+  :description "Remote TTY backends for hexagramMUSH"
+  :url "https://github.com/hexagram30/terminal"
+  :license {
+    :name "Apache License, Version 2.0"
+    :url "http://www.apache.org/licenses/LICENSE-2.0"}
+  :dependencies [
+    [org.clojure/clojure "1.8.0"]])
