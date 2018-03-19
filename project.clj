fa58acb8419e2a62d9addec8dc7ca34e636b9fbb
Duncan McGreggor <oubiwann@gmail.com>
2018-03-18 21:38:44 -0500

    AOT and dev env tweaks.

--- project.clj
+++ project.clj
@@ -1,3 +1,19 @@
+(defn get-banner
+  []
+  (try
+    (str
+      (slurp "resources/text/banner.txt")
+      (slurp "resources/text/loading.txt"))
+    ;; If another project can't find the banner, just skip it;
+    ;; this function is really only meant to be used by Dragon itself.
+    (catch Exception _ "")))
+
+(defn get-prompt
+  [ns]
+  (str "\u001B[35m[\u001B[34m"
+       ns
+       "\u001B[35m]\u001B[33m λ\u001B[m=> "))
+
 (defproject hexagram30/terminal "0.1.0-SNAPSHOT"
   :description "Telnet, SSL Telnet, SSH, and secure REPLs for use by hexagram30 projects"
   :url "https://github.com/hexagram30/terminal"
@@ -16,7 +32,8 @@
   :profiles {
     :ubercompile {
       :aot [hxgm30.terminal.telnet.handler
-            hxgm30.terminal.telnet.initializer]}
+            hxgm30.terminal.telnet.initializer
+            hxgm30.terminal.telnet.server]}
     :test {
       :plugins [[lein-ltest "0.3.0"]]}
     :telnet {
@@ -25,6 +42,15 @@
     "ubercompile" ["do"
       ["clean"]
       ["with-profile" "+ubercompile" "compile"]]
+    "install" ["do"
+      ["clean"]
+      ["with-profile" "+ubercompile" "install"]]
+    "jar" ["do"
+      ["clean"]
+      ["with-profile" "+ubercompile" "jar"]]
+    "uberjar" ["do"
+      ["clean"]
+      ["with-profile" "+ubercompile" "uberjar"]]
     "telnet-server" ["do"
       ["ubercompile"]
       ["with-profile" "+telnet" "run"]]})
