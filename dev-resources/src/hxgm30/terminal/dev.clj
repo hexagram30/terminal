2ed2a0a52635a2e2f1fa2d7c79593ea01586da19
Duncan McGreggor <duncan.mcgreggor@nasa.gov>
2018-04-15 13:11:33 -0500

    Added note/error log about reloading.

--- dev-resources/src/hxgm30/terminal/dev.clj
+++ dev-resources/src/hxgm30/terminal/dev.clj
@@ -9,6 +9,7 @@
     [com.stuartsierra.component :as component]
     [hxgm30.terminal.components.config :as config]
     [hxgm30.terminal.components.core]
+    [taoensso.timbre :as log]
     [trifl.java :refer [show-methods]])
   (:import
     (java.net URI)
@@ -21,8 +22,8 @@
 (logger/set-level! '[hxgm30] :debug)
 
 (def ^:dynamic *mgr* nil)
-
 (def init-component "hxgm30.terminal.components.core")
+(def after-refresh 'hxgm30.terminal.dev/startup)
 
 (defn banner
   []
@@ -79,10 +80,19 @@
 ;;;   Reloading Management   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(def reloading-unsupported
+  (str "The reloading functions don't work with this project. This is due to "
+       "the use of 'gen-class'ed code in the system components, and the fact "
+       "that those components are used in this namespace to start up the "
+       "system."))
+
 (defn reset
   []
-  (shutdown)
-  (repl/refresh :after 'hxgm30.graphdb.dev/startup))
+  ; (shutdown)
+  ; (repl/refresh :after after-refresh)
+  (log/error reloading-unsupported))
 
-(def refresh #'repl/refresh)
-
+;(def refresh #'repl/refresh)
+(defn refresh
+  []
+  (log/error reloading-unsupported))
